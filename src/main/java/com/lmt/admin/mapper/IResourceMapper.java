7a999c56d29862845a7eb6dafd66ad1b7e953fca
悲情黑客 <adu003@163.com>
2017-08-18 10:17:59 +0800

    添加根据管理员id查寻资源列表接口， 添加根据角色id查寻资源列表接口

--- src/main/java/com/lmt/admin/mapper/IResourceMapper.java
+++ src/main/java/com/lmt/admin/mapper/IResourceMapper.java
@@ -1,5 +1,8 @@
 package com.lmt.admin.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
 import org.mybatis.spring.annotation.MapperScan;
 
 import com.lmt.admin.model.Resource;
@@ -13,4 +16,18 @@ import com.lmt.orm.mybatis.mapper.IBaseMapper;
 @MapperScan
 public interface IResourceMapper extends IBaseMapper<Resource, Integer> {
 
+	/**
+	 * 根据角色id查寻资源列表
+	 * @param roleId
+	 * @return
+	 */
+	public List<Resource> listByRoleId(@Param(value = "roleId") int roleId);
+	
+	/**
+	 * 根据管理员id查寻该管理员的资源列表
+	 * @param adminId
+	 * @return
+	 */
+	public List<Resource> listByAdminId(@Param(value = "adminId") int adminId);
+	
 }
