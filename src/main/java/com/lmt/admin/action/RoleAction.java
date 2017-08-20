3fcd215438f92c3491e7596f491657d46f98cd1b
悲情黑客 <adu003@163.com>
2017-08-20 19:15:00 +0800

    添加编辑角色、角色查寻接口

--- src/main/java/com/lmt/admin/action/RoleAction.java
+++ src/main/java/com/lmt/admin/action/RoleAction.java
@@ -16,6 +16,7 @@ import com.lmt.admin.service.IResourceService;
 import com.lmt.admin.service.IRoleResourceService;
 import com.lmt.admin.service.IRoleService;
 import com.lmt.common.action.BaseAction;
+import com.lmt.orm.common.model.PaginationModel;
 
 /**
  * 
@@ -51,7 +52,11 @@ public class RoleAction extends BaseAction {
 			@RequestParam(value="limit",defaultValue="10") int limit,
 			@RequestParam(value="keyword",required=false) String keyword,
 			HttpServletRequest request,HttpServletResponse response){
-		
+		PaginationModel<Role> pageModel = new PaginationModel<Role>();
+		pageModel.setCurrPage(1);
+		pageModel.setLimit(limit);
+		pageModel = roleService.queryByPagination(pageModel);
+		request.setAttribute("pageModel", pageModel);
 		return "admin/role/list";
 	}
 	
@@ -82,11 +87,46 @@ public class RoleAction extends BaseAction {
 	@RequestMapping(value="/save",name="保存角色")
 	@ResponseBody
 	public Map<String,Object> save(
+			@RequestParam(value="id",defaultValue="0") int id,
+			@RequestParam(value="name") String name,
+			@RequestParam(value="status") int status,
+			@RequestParam(value="description") String description,
 			HttpServletRequest request,HttpServletResponse response){
 		Map<String,Object> map = new HashMap<String, Object>();
 		map.put("success", true);
 		map.put("code", 200);
-		
+		Role role = roleService.get(id);
+		if(role == null){
+			role = new Role();
+			role.setName(name);
+			role = roleService.load(role);
+			if(role != null){
+				map.put("success", true);
+				map.put("code", 201);
+				map.put("msg", "相同角色名存在");
+				return map;
+			}
+			role = new Role();
+			role.setName(name);
+			role.setStatus(status);
+			role.setDescription(description);
+			roleService.insert(role);
+			map.put("msg", "添加成功");
+		}else{
+			Role role1 = new Role();
+			role1.setName(name);
+			role1 = roleService.load(role1);
+			if(role1 != null && id != role1.getId().intValue()){
+				map.put("success", true);
+				map.put("code", 201);
+				map.put("msg", "相同角色名存在");
+				return map;
+			}
+			role.setDescription(description);
+			role.setName(name);
+			roleService.update(role);
+			map.put("msg", "修改成功");
+		}
 		return map;
 	}
 	
