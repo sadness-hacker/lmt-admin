7184b56a88481067db3656a357d5189e0b3732ac
悲情黑客 <adu003@163.com>
2017-08-22 14:49:29 +0800

    新增查寻接口

--- src/main/java/com/lmt/admin/mapper/IRoleResourceMapper.java
+++ src/main/java/com/lmt/admin/mapper/IRoleResourceMapper.java
@@ -1,5 +1,6 @@
 package com.lmt.admin.mapper;
 
+import org.apache.ibatis.annotations.Param;
 import org.mybatis.spring.annotation.MapperScan;
 
 import com.lmt.admin.model.RoleResource;
@@ -14,4 +15,14 @@ import com.lmt.orm.mybatis.mapper.IBaseMapper;
 @MapperScan
 public interface IRoleResourceMapper extends IBaseMapper<RoleResource, Integer> {
 
+	/**
+	 * 根据管理员id，资源id获取权限角色资源对象
+	 * @param adminId
+	 * @param resourceId
+	 * @return
+	 */
+	public RoleResource getByAdminIdResId(
+			@Param(value = "adminId") int adminId,
+			@Param(value = "resourceId") int resourceId);
+	
 }
